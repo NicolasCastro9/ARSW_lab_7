a8a32c57659c3d0f1b1077637788839a0abe0d10
NicolasCastro9 <tankiandown@gmail.com>
2023-10-14 15:41:35 -0500

    contenido lab6

---
+++ ARSW_lab_7/target/classes/static/js/apiclient.js
@@ -0,0 +1,16 @@
+var apiclient = (function(){
+    var url='http://localhost:8080';
+    return {
+        getBlueprintsByAuthor: function (authname, callback) {
+            $.get(url + "/blueprints/" + authname, function (data) {
+                callback(data);
+            });
+        },
+
+        getBlueprintsByNameAndAuthor: function (authname, bpname, callback) {
+            $.get(url + "/blueprints/" + authname + "/" + bpname, function (data) {
+                callback(data);
+            });
+        }
+    }
+})();
